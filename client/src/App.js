060dcbf56c8855e9cf168471ab6e2312991e661b
msolorzano81 <msolorzano81@outlook.com>
2018-07-27 18:54:07 -0400

    Revert "some stuffff"

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 import Signup from './Signup';
 import Login from './Login';
 import BackgroundImage from './components/bgimage';
-import Navbar from "./components/Navbar";
 //import Subscriptions from './pages/subscriptions';
 import SubCard from './components/SubCard';
 import NavSignout from './components/NavBar_signout'
@@ -30,13 +29,6 @@ class App extends Component {
   render() {
     return (
       <Router>
-         <div>
-        <Route path="/login" render={()=>(
-          <div>
-            <Login/>
-            <Navbar/>
-            </div> 
-        )} />
         <div>
         <Route exact path="/" render={() => (
           <div>
@@ -83,33 +75,5 @@ class App extends Component {
         
 
 
-
-        <Route path="/signup" render={()=>(
-          <div>
-            <Signup/>
-            <Navbar/>
-          </div> 
-        )} />
-
-        <Route path="/home" render={()=>(
-          <div>
-            <Navbar/>
-            <BackgroundImage/>
-            </div>
-           
-        )} />
-          <Route path="/subscriptions" render={()=>(
-          <div>
-            <Subscriptions/>
-            <Navbar/>
-            </div>
-           
-        )} />
-       </div> 
-</Router>
- );
-}   
-}   
-   
 
 export default App;
