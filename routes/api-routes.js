7420ee6139a785d6d61e85a82ae15f25a305449d
Raven <raven.bell@yahoo.com>
2018-07-27 23:20:08 -0400

    change API routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -19,7 +19,7 @@ module.exports = function(app) {
 // Get all subscriptions route
     app.get('/api/all', (req, res) => {
       console.log("hello from api/all")
-        db.subscriptions.findAll({}).then((results)=> {
+        db.Subscriptions.findAll({}).then((results)=> {
             console.log(results);
             res.json(results);
         })
@@ -28,7 +28,7 @@ module.exports = function(app) {
 
 // Get subscription box by category
     app.get("/api/posts/category/:category", function(req, res) {
-      db.subscriptions.findAll({
+      db.Subscriptions.findAll({
         where: {
           category: req.params.category
         }
@@ -40,7 +40,7 @@ module.exports = function(app) {
 
     // app.get('/api/create', (req, res) => {
     //   console.log("hello from api/all")
-    //     db.subscriptions.create({
+    //     db.Subscriptions.create({
     //       name: "Hello3",
     //       description: "Found me",
     //       link: "www.heythere.com",
@@ -58,7 +58,7 @@ module.exports = function(app) {
 
     app.post('/api/posts', (req, res) =>{
       console.log("we hit the route!!!!", req.body)
-      db.subscriptions.create({
+      db.Subscriptions.create({
         name: "",
         description: "",
         link: "",
